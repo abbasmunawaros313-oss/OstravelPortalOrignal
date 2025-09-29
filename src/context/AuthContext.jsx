17a16dc98e249b5bede3d46bd0d96ab68f29d4b1
abbasmunawaros313-oss <abbasmunawaros313@gmail.com>
2025-09-29 14:04:04 +0500

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -142,7 +142,7 @@ export const AuthProvider = ({ children }) => {
     try {
       checkRateLimit();
 
-      if (email === "adminos@gmail.com" && password === "ospk123") {
+      if (email === "munawar@gmail.com" && password === "Kaneez143") {
         const adminUser = { email, uid: "local-admin" };
         setUser(adminUser);
         setIsAdmin(true);
