512410d873c38828e36af60e564b73ffc281e107
abbasmunawaros313-oss <abbasmunawaros313@gmail.com>
2025-10-17 12:39:08 +0500

    Update AdminHome.jsx

--- src/Pages/AdminHome.jsx
+++ src/Pages/AdminHome.jsx
@@ -254,35 +254,14 @@ export default function AdminHome() {
 
           <div className="flex gap-3 w-full md:w-auto">
             <div className="relative w-full md:w-96">
-              <FaSearch className="absolute left-3 top-3 text-slate-400" />
-              <input
-                value={globalSearch}
-                onChange={(e) => setGlobalSearch(e.target.value)}
-                placeholder="Search bookings, passport, employee email..."
-                className="w-full pl-10 pr-4 py-3 rounded-xl shadow-sm border border-slate-200 focus:border-blue-300 outline-none"
-              />
+             
+              <Link to="/detailed-search">
+                 <button className="px-6 py-3 border rounded-lg text-white bg-gray-500 font-xl cursor-pointer font-bold">
+                     Search All
+                 </button>
+              </Link>
             </div>
-            <button
-              onClick={() =>
-                exportToCSV(
-                  `bookings_export_${new Date().toISOString().slice(0, 10)}.csv`,
-                  bookings.map((b) => ({
-                    id: b.id,
-                    fullName: b.fullName,
-                    passport: b.passport,
-                    country: b.country,
-                    visaType: b.visaType,
-                    userEmail: b.userEmail,
-                    totalFee: b.totalFee,
-                    receivedFee: b.receivedFee,
-                    date: b.date,
-                  }))
-                )
-              }
-              className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white px-4 py-2 rounded-xl shadow hover:brightness-95 flex items-center gap-2"
-            >
-              <FaFileCsv /> Export
-            </button>
+            
           </div>
         </div>
         {/* Summary Quick Cards */}
